c53f4ae94c6fd86f3851b744f7255d15ba72aa97
Maxim Grossu <maxgrossu@yandex.ru>
2023-07-27 17:39:11 +0300

    fix: исправлен баг с ключом токена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
-// const { JWT_SECRET } = process.env.JWT_SECRET;
+const { JWT_SECRET } = process.env.JWT_SECRET;
+const { NODE_ENV } = process.env.NODE_ENV;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
-    payload = jwt.verify(token, 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
     return;
